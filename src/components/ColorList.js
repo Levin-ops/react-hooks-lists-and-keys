a52bce832014720c554063310fec08059c1edf7f
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-25 15:33:02 +0300

    redone

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,26 +1,25 @@
 import React from "react";
 
-function ColorList() {
+function ColorList(){
   const colors = [
-    "firebrick",
-    "rebeccapurple",
-    "salmon",
-    "darkslategray",
-    "hotpink",
+    'firebrick',
+    'rebeccapurple',
+    'salmon',
+    'darkslategray',
+    'hotpink'
   ];
 
+  const colorElement = colors.map((color) => {
+    return <li key={color} style={{color:color}}>{color}</li>;
+  })
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
+
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+       {colorElement}
       </ol>
     </div>
-  );
+  )
 }
-
-export default ColorList;
+export default ColorList
